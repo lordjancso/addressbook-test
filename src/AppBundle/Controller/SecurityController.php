abae1fbe5e4965778af15b86c5fe696d9995815e
lordjancso <lordjancso@gmail.com>
2015-11-10 10:24:08 +0100

    Template skeletons

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ));
+    }
+}
