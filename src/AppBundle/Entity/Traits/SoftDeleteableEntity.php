d055d55a444eb0c7e6d985cd3eb52872338ff38b
lordjancso <lordjancso@gmail.com>
2015-11-10 09:29:19 +0100

    User entity initialized

---
+++ src/AppBundle/Entity/Traits/SoftDeleteableEntity.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Entity\Traits;
+
+trait SoftDeleteableEntity
+{
+    /**
+     * @ORM\Column(name="deleted_at", type="datetime", nullable=true)
+     */
+    protected $deletedAt;
+
+    /**
+     * Sets deletedAt.
+     *
+     * @param \Datetime|null $deletedAt
+     *
+     * @return $this
+     */
+    public function setDeletedAt(\DateTime $deletedAt = null)
+    {
+        $this->deletedAt = $deletedAt;
+
+        return $this;
+    }
+
+    /**
+     * Returns deletedAt.
+     *
+     * @return \DateTime
+     */
+    public function getDeletedAt()
+    {
+        return $this->deletedAt;
+    }
+
+    /**
+     * Is deleted?
+     *
+     * @return bool
+     */
+    public function isDeleted()
+    {
+        return null !== $this->deletedAt;
+    }
+}
---
+++ src/AppBundle/Entity/Traits/TimestampableEntity.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity\Traits;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+
+trait TimestampableEntity
+{
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(name="created_at", type="datetime", nullable=false)
+     */
+    protected $createdAt;
+
+    /**
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(name="updated_at", type="datetime", nullable=false)
+     */
+    protected $updatedAt;
+
+    /**
+     * Sets createdAt.
+     *
+     * @param \DateTime $createdAt
+     * @return $this
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Returns createdAt.
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Sets updatedAt.
+     *
+     * @param \DateTime $updatedAt
+     * @return $this
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Returns updatedAt.
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
